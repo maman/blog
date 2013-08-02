3d8b479e8345d966e8c38da4f62d36d114b059de
Achmad Mahardi <achmad@mahardi.me>
2013-08-03 02:51:59 +0700

    preliminary print support

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,20 @@
 
 */
 
+// $(document).ready(function() {
+// 	var linkTemplate = "<div class='entry-content'><ol class='link-list'></ol></div>";
+// 	$(linkTemplate).insertAfter('.entry-content');
+// 	appendLinks();
+// });
 
-
-
-
+// function appendLinks() {
+// 	$('.entry-content a').each(function(linkId) {
+// 		var oldLink = $(this).attr('href');
+// 		// var linkHref = $(this).attr('href');
+// 		var count = linkId + 1;
+// 		$(this).attr('href', '#d' + count);
+// 		$(this).addClass('is-link');
+// 		$(this).append("<span class='top'>["+count+"]</span>");
+// 		$('.link-list').append("<li id='d"+count+"'><a href='"+oldLink+"'>"+oldLink+"</a>");
+// 	});
+// }
