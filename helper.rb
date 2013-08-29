399f7728c086c3cda506f549724410339ed2c9c8
Achmad Mahardi <achmad@mahardi.me>
2013-08-29 23:08:04 +0700

    fixing PJAX-3

--- helper.rb
+++ helper.rb
@@ -14,14 +14,6 @@ module Toto
 	class Article < Hash
 		include Template
 
-		def link()  self[:link]	end
-
-		def linktitle
-			alias :link link
-			doc = Hpricot(open(link))
-			(doc/"title").inner_text
-		end
-
 		def dateUTC
 			lambda {|now| now.strftime("%Y-%m-%d") }.call(self[:date])
 		end
